ccf7f0d2d8db7ce55925f67f2a46e8735bc45acc
SHPZ <shaaapz@gmail.com>
2026-08-11 21:42:23 +0800

    功能: 移除技术验证工程

--- spike/KimiPetSpike/Source/KimiPetSpike/KimiPetSpike.cpp
+++
@@ -1,6 +0,0 @@
-#include "KimiPetSpike.h"
-#include "Modules/ModuleManager.h"
-
-DEFINE_LOG_CATEGORY(LogKimiPetSpike);
-
-IMPLEMENT_PRIMARY_GAME_MODULE(FDefaultGameModuleImpl, KimiPetSpike, "KimiPetSpike");
